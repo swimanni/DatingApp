6ccf649eb61ee8ff9c1dc14d2a6cc8d76c30bb2f
Anirudh <swimanni@gmail.com>
2024-07-21 17:49:01 +0530

    Login with token

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using API.Controllers;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class UsersController(DataContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        var users = await context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUsers(int id){
+        var user = await context.Users.FindAsync(id);
+        if(user==null) return NotFound();
+        return user;
+    }
+}
